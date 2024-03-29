9d6ec0921c8f7b63363a67b771492a7086dc2802
mymircle0118 <noahsflood908@gmail.com>
2024-03-30 04:43:05 +0900

    vercel fix error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import imp from 'vite-plugin-imp';
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    imp({
+      libList: ['axios'],
+    })
+  ],
+  
   // resolve: {
   //   alias: [
   //     { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
